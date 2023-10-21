95647451900b24c44fd29f7a842f56c0458fe889
bakhtiarmuhib <48671856+bakhtiarmuhib@users.noreply.github.com>
2023-10-21 21:22:04 +0600

    update

---
+++ simple problem solve/summationOfSquareNumber.py
@@ -0,0 +1,48 @@
+# 1**2 + 2**2 + 3^2 + 4**2
+
+n = 4
+
+summation =   (n*(n+1)*(2*n+1))/6
+print("summation is:",summation)
+
+
+# 1+3+5+7+9+11
+
+a = 1
+n = 9
+d = 2 # d = 3-1 
+
+# n th number
+_9thNumber = a + (9-1) * d
+
+print(_9thNumber)
+
+summation = (n/2)*(2*a + (n-1)*d) 
+print(summation)
+
+
+# 1**3 + 2**3 + 3^3 + 4**3
+
+n = 4
+summation = ((n*(n+1))/2)**2
+print(summation)
+
+
+#############################################
+#  1+3+9+27
+a= 1
+r = 2/1
+
+r > 1
+
+product = (a*((3**4)-1))/(3-1)
+print("product:",product)
+
+r < 1
+
+#  a(1-q^n)/(1-q)
+
+# leap year
+# Armstrong number
+
+
